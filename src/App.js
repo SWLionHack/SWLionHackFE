07e633cd51985bdf505161bcac0683dec19e5576
vkflco08 <vkflco08@naver.com>
2024-07-23 18:24:06 +0900

    fix build error #2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Footer from './components/common/Footer';
 import NavBar from './components/common/NavBar';
 import MainPage from './components/MainPage';
 import Diary from './components/Diary';
 import Survey from './components/Survey';
 import Community from './components/Community';
 import QnA from './components/QnA';
-import Counseling from './components/Counseling';
 import NearbyCounseling from './components/NearbyCounseling';
 import Login from './components/login/Login';
 import Register from './components/login/Register';
 import { AuthProvider } from './components/login/AuthContext';
-import ProtectedRoute from './components/login/ProtectedRoute';
+// import Footer from './components/common/Footer';
+// import Counseling from './components/Counseling';
+// import ProtectedRoute from './components/login/ProtectedRoute';
 import './App.css';
 
 // function App() {
